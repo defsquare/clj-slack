3d1ae2523ae7a34e69febbc944244ae8486b3116
jeremiegrodziski <jeremie@grodziski.com>
2021-04-22 19:09:44 +0200

    Add upload files

--- README.md
+++ README.md
@@ -7,17 +7,18 @@ A wrapper around the [Slack Java SDK](https://github.com/slackapi/java-slack-sdk
 ```clojure
 
 require [slack.core :as slack :refer [post-msg!]]
-(post-msg! "xoxb-494114936464-1996087116864-kNtMFXgLu27J7N8qQp3CIb9A" "team-galaxie" "test message")
+(post-msg! "xoxb-494114936464-xxx" "team-xxx" "test message")
 
 s.core> 
 ```
 
 ```bash
-clojure -Sdeps {:deps {:git/url "https://github.com/defsquare/clj-slack" :sha "8324315d6fb9e1dc7d44b366f3e03fbed2fb2c42"}} -m slack.core --token "xoxb-594114936464-1996087116864-kNtMFXgLu27J7N8qQp3CIb9B" --channel "team-galaxie" --message "another test message"
+clj -Sdeps '{:deps {clj-slack/clj-slack {:git/url "https://github.com/defsquare/clj-slack" :sha "5f1703f20a56dd04f9d8cc2db951f8073245cc26"}} :aliases {:post-msg {:exec-fn slack.core/post-msg!}}}' -Xpost-msg :token '"xoxb-494114936464-199xxxx"' :channel '"team-xxx"' :message '"hello world"'
+
 ```
 
 ## License
 
-Copyright © 2021 Jeremiegrodziski
+Copyright © 2021 Defsquare
 
 Distributed under the Eclipse Public License version 1.0.
--- src/slack/core.clj
+++ src/slack/core.clj
@@ -1,12 +1,14 @@
 (ns slack.core
-  (:require [cli-matic.core :refer [run-cmd]])
+  (:require [cli-matic.core :refer [run-cmd]]
+            [clojure.java.data :as jd])
   (:import [com.slack.api Slack]
            [com.slack.api.methods.request.chat ChatPostMessageRequest]
            [com.slack.api.model Attachment]
-           [com.slack.api.methods.response.chat ChatPostMessageResponse]))
+           [com.slack.api.methods.response.chat ChatPostMessageResponse]
+           [com.slack.api.methods.request.files FilesUploadRequest]
+           [com.slack.api.bolt App AppConfig]))
 
 (def slack (Slack/getInstance))
-
 
 (def cli-opts
   [{:as "Slack token"
@@ -22,8 +24,9 @@
     :type :string}])
 
 (defn post-msg!
-  ([{:keys [token channel message attachments] :as args}]
-   (post-msg! token channel message attachments))
+  ([{:keys [token channel message attachment-filename] :as args}]
+   (println (format "Post message to channel %s with attachment %s" channel attachment-filename))
+   (post-msg! token channel message [{:pretext attachment-filename :text (slurp attachment-filename)}]))
   ([token channel message attachments]
    (let [attachments-list (java.util.ArrayList.)
          _                (doseq [{:keys [pretext text] :as attachment} attachments]
@@ -39,6 +42,36 @@
          response (-> slack
                       (.methods token)
                       (.chatPostMessage request))]
+     (clojure.pprint/pprint (jd/from-java response))
+     response)))
+
+(defn upload-file!
+  ([{:keys [token filename initial-comment channel] :as args}]
+   (println (format "Upload file %s to channel %s" filename channel))
+   (upload-file! token filename initial-comment [channel]))
+  ([token filename initial-comment channels]
+   (let [channels-list (java.util.ArrayList.)
+         _             (doseq [channel channels]
+                         (.add channels-list channel))
+         f        (java.io.File. filename)
+         file-data (let [ary (byte-array (.length f))
+                         is (java.io.FileInputStream. f)]
+                     (.read is ary)
+                     (.close is) ary)
+         request (-> (FilesUploadRequest/builder)
+                     (.content (slurp filename))
+                     (.file f)
+                     (.fileData file-data)
+                     (.filename filename)
+                     (.token token)
+                     (.title filename)
+                     (.initialComment initial-comment)
+                     (.channels channels-list)
+                     (.build))
+         response (-> slack
+                      (.methods token)
+                      (.filesUpload request))]
+     (clojure.pprint/pprint (jd/from-java response))
      response)))
 
 
@@ -56,12 +89,9 @@
 (defn -main [& args]
   (run-cmd args CLI_CONFIG))
 
-"xapp-1-A01ULDGPCAF-1965474607862-de4f35260aed2f0c8aa3b9c61b3a408f01580fa1e10fc5474095bb9d5e6451b1"
-"xoxb-494114936464-1996087116864-kNtMFXgLu27J7N8qQp3CIb9A"
-
 (comment
   (post-msg!
-             "xoxb-494114936464-1996087116864-kNtMFXgLu27J7N8qQp3CIb9A"
+             "xoxb-494114936464-xxxx"
              "team-galaxie"
              "text"
              "attachment text"))
