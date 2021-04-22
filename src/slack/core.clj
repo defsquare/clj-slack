5f1703f20a56dd04f9d8cc2db951f8073245cc26
jeremiegrodziski <jeremie@grodziski.com>
2021-04-22 16:00:30 +0200

    Add attachemnt to pst msg

--- src/slack/core.clj
+++ src/slack/core.clj
@@ -2,6 +2,7 @@
   (:require [cli-matic.core :refer [run-cmd]])
   (:import [com.slack.api Slack]
            [com.slack.api.methods.request.chat ChatPostMessageRequest]
+           [com.slack.api.model Attachment]
            [com.slack.api.methods.response.chat ChatPostMessageResponse]))
 
 (def slack (Slack/getInstance))
@@ -20,15 +21,25 @@
     :option "message"
     :type :string}])
 
-(defn post-msg! [token channel message]
-  (let [request  (-> (ChatPostMessageRequest/builder)
-                     (.channel channel)
-                     (.text message)
-                     (.build))
-        response (-> slack
-                     (.methods token)
-                     (.chatPostMessage request))]
-    response))
+(defn post-msg!
+  ([{:keys [token channel message attachments] :as args}]
+   (post-msg! token channel message attachments))
+  ([token channel message attachments]
+   (let [attachments-list (java.util.ArrayList.)
+         _                (doseq [{:keys [pretext text] :as attachment} attachments]
+                            (.add attachments-list (-> (Attachment/builder)
+                                                       (.pretext pretext)
+                                                       (.text text)
+                                                       (.build))))
+         request  (-> (ChatPostMessageRequest/builder)
+                      (.channel channel)
+                      (.text message)
+                      (.attachments attachments-list)
+                      (.build))
+         response (-> slack
+                      (.methods token)
+                      (.chatPostMessage request))]
+     response)))
 
 
 (defn post-msg-cli! [{:keys [token channel message] :as args}]
@@ -45,5 +56,12 @@
 (defn -main [& args]
   (run-cmd args CLI_CONFIG))
 
+"xapp-1-A01ULDGPCAF-1965474607862-de4f35260aed2f0c8aa3b9c61b3a408f01580fa1e10fc5474095bb9d5e6451b1"
+"xoxb-494114936464-1996087116864-kNtMFXgLu27J7N8qQp3CIb9A"
 
-
+(comment
+  (post-msg!
+             "xoxb-494114936464-1996087116864-kNtMFXgLu27J7N8qQp3CIb9A"
+             "team-galaxie"
+             "text"
+             "attachment text"))
